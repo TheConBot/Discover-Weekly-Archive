1e53271b000a803d015baee380f60f58ffa44e21
cbotts-forte <cbotts@forte.io>
2022-07-08 22:10:38 -0400

    added readme

--- Discover Weekly Archive/DiscoverWeeklyArchiveService.cs
+++ Discover Weekly Archive/DiscoverWeeklyArchiveService.cs
@@ -9,9 +9,6 @@ using Microsoft.Extensions.Hosting;
 
 namespace Discover_Weekly_Archive
 {
-    /// <summary>
-    /// This is a tool to replace the functionality that was available via the IFTTT applet where everytime your Discover Weekly is updated, it adds all then new tracks to a seperate playlist to save them as the Discover Weekly playlist overwrites itself every week. For the underlying boilerplate services that enable interaction with Spotify via it's API, I borrowed heavily from https://github.com/JohnnyCrazy/Sp0.
-    /// </summary>
     public class DiscoverWeeklyArchiveService : IDiscoverWeeklyArchiveService
     {
         private ISpotifyService spotifyService;
---
+++ README.md
@@ -0,0 +1,6 @@
+# Discover Weekly Archive
+
+This is a tool to replace the functionality that was available via the IFTTT applet where every time your Discover Weekly is updated, it adds all then new tracks to a separate playlist to save them as the Discover Weekly playlist overwrites itself every week. For the underlying boilerplate services that enable interaction with Spotify via it's API, I borrowed heavily from https://github.com/JohnnyCrazy/Sp0.
+
+# Installation
+TODO
